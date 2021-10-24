d22e60dbdfb97d56074f3417501755ab4eb2886c
Amber Johnson <amjohnson10@waketech.edu>
2021-10-24 09:09:58 -0400

    Added test name

---
+++ search/test_unit10.py
@@ -0,0 +1,12 @@
+# Amber Johnson
+# CSC256.0002 FA21
+import requests
+import pytest
+
+url_ddg = "https://api.duckduckgo.com"
+
+
+def test_ddg0():
+    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
+    rsp_data = resp.json()
+    assert "DuckDuckGo" in rsp_data["Heading"]
