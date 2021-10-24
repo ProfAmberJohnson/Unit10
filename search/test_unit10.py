fb62a3ad85f8a24d45cc850b1537e9219b2783ac
Amber Johnson <amjohnson10@waketech.edu>
2021-10-24 11:14:15 -0400

    Added president testing

--- search/test_unit10.py
+++ search/test_unit10.py
@@ -6,7 +6,20 @@ import pytest
 url_ddg = "https://api.duckduckgo.com"
 
 
-def test_ddg0():
-    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
+@pytest.mark.parametrize("president", ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Buren", "Harrison",
+                                       "Tyler", "Polk", "Fillmore", "Pierce", "Buchanan", "Lincoln", "Johnson", "Hayes",
+                                       "Garfield", "Arthur", "Cleveland", "McKinley", "Roosevelt", "Taft", "Harding",
+                                       "Coolidge", "Hoover", "Truman", "Eisenhower", "Kennedy", "Nixon", "Ford",
+                                       "Carter", "Reagan", "Bush", "Clinton", "Obama", "Trump", "Biden"])
+def test_presidents(president):
+    resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json&pretty=1")
     rsp_data = resp.json()
-    assert "DuckDuckGo" in rsp_data["Heading"]
+    topics_list = []
+    for topic in rsp_data["RelatedTopics"]:
+        topics_list.append(topic["FirstURL"])
+    if any(president in string for string in topics_list):
+        assert True
+    else:
+        assert False
+
+
